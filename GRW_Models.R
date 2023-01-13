ab224050db74f0833c60ec6bd69ae0402a36df18
Jessica Vasconcelos de Abreu <38982933+JessicaVasconcelos@users.noreply.github.com>
2023-01-13 14:54:42 -0300

    Update GRW_Models.R

--- GRW_Models.R
+++ GRW_Models.R
@@ -118,6 +118,7 @@ golden2 <- function(y, x, xvarglobal, weight, lat, long, output, method, model=G
       yhat <- matrix(0,1,1)
       output <<- hv[,"h"]
       # aqui faltam os laços sobre os quais eu perguntei ao professor
+      # resposta: remover adptiven
     }
     if(method=="fixed_g"|method=="fixed_bsq"|method=="adaptive_bsq"){
     }
@@ -186,7 +187,7 @@ golden2 <- function(y, x, xvarglobal, weight, lat, long, output, method, model=G
             W_f <- cbind(matrix(i,n,1),w,seq(1,nrow(w)))
           }
           else{
-            W_f <- cbind(W_f,c(rbind(matrix(i,n,1),w,seq(1,nrow(w)))))
+            W_f <- cbind(W_f,c(rbind(matrix(i,n,1),w,t(seq(1,nrow(w)))))) #revisar linha
           }
           if(is.null(xvarglobal)){
             ba <- solve(t(t(xa)%*%diag(1,n,n)-hat_matrix))%*%(diag(1,n,n)-hat_matrix)%*%(xa*wt)%*%t(xa)%*%(t(diag(1,n,n)-hat_matrix))%*%(diag(1,n,n)-hat_matrix)*(y*wt) 
@@ -234,13 +235,149 @@ golden2 <- function(y, x, xvarglobal, weight, lat, long, output, method, model=G
       Ai <- uj*(1-uj)
       Ai <- ifelse(Ai<=0,E*(-5),Ai)
       zj <- nj+(y-uj)/Ai
-#continua...
-      
+      if (det(t(x)%*%(w*Ai*x*wt))==0){
+        b <- matrix(0,nvar,1)
+      }
+      else{
+        b <- solve(t(x)%*%(w*Ai*x*wt))*t(x)%*%(w*Ai*wt*zj)
+      }
+      nj <- x%*%b
+      nj <- ifelse(nj>E^2,E^2,nj)
+      uj <- exp(nj)/(1+exp(nj))
+      olddev <- dev
+      uj <- ifelse(uj<E^(-150),E^(-150),uj)
+      tt <- y/uj
+      tt < ifelse(tt==0,E^(-10),tt)
+      uj <- ifelse(uj==1,0.99999,uj)
+      tt2 <- ifelse(tt2==0,E^-10,tt2)
+      dev <- 2*sum(y*log(tt))+(1-y)*log(tt2)
+      if(cont>100){
+        ddev <- 0.0000001
+      }
+      else{
+        ddev <- dev-olddev
+      }
+    } #fecha while
+    if(method=="fixed_g"|method=="fixed_bsq"|method=="adaptive_bsq"){
+      yhat[i] <- uj[i]
+      if(det(t(x)%*%(w*Ai*x*wt)==0)){
+        S[i] <- 0
+      }
+      else{
+        S[i] <- (x[i,]%*%solve(t(x)%*%(w*Ai*x*wt))*t((x*w*wt*Ai)))[i]
+      }
+      if(is.null(xvarglobal)){
+        if(i==1){
+          W_f <- cbind(matrix(i,n,1),w,t(seq(1,nrow(w))))
+        }
+        else{
+          W_f <- rbind(W_f,c(cbind(matrix(i,n,1),w,t(seq(1,nrow(w))))))
+        }
+      }
+      if(is.null(xvarglobal)){
+        uj <- (y+y[,])/2
+        nj <- log(uj/(1-uj))
+        dev <- 0; ddev <- 1; cont <-1
+      }
+      while(abs(ddev)>0.000001 & cont<100){
+        cont <- cont+1
+        uj <- ifelse(uj>E^100,E^100,uj)
+        Aa <- uj*(1-uj)
+        Aa <- ifelse(Aa <= 0,E^-5,Aa)
+        za <- nj+(y-uj)/Aa
+        if(det(t(xa)%*%(Aa*xa*wt))==0){
+          ba <- matrix(0,nvar,1)
+        }
+        else{
+          ba <- solve(t(xa)%*%(Aa*xa*wt))%*%t(xa)*(Aa*za*wt)
+        }
+        nj <- xa%*%ba
+        nj <- ifelse(nj>E^2,E^2,nj)
+        uj <- exp(nj)/(1+exp(nj))
+        olddev <- dev
+        uj <- ifelse(uj<E^-150,E^-150,uj)
+        tt <- y/uj
+        tt <- ifelse(tt=0,E^-10,tt)
+        uj <- ifelse(uj==1,0.99999,uj)
+        tt2 <- (1-y)/(1-uj)
+        tt2 <- ifelse(tt2==0,E^-10,tt2)
+        dev <- 2*sum((y*log(tt))+(1-y)*log(tt2))
+        ddev <- dev-olddev
+      } #fecha while
+      diffba <- 1; contb <- 1
+    } #fecha method
+    while(abs(diffba)>0.00001 & contb<50){
+      oldba <- ba
+    for(i in 1:n) {
+      m1 <- (i-1)*n+1
+      m2 <- m1+(n-1)
+      w <- w_f[m1:m2,2] 
+      dev <- 0; ddev <- 1; cont2<- 0
+      while(abs(ddev)>0.000001 & cont2<100){
+        uj <- ifelse(uj>E^100,E^100,uj)
+        Ai <- uj*(1-uj)
+        Ai <- ifelse(Ai<=0,E^-5,Ai)
+        zj <- nj+(y-uj)/Ai-xa%*%ba
+        if(det(t(x)%*%(w*Ai*x*wt))==0){
+          b <- matrix(0,nvar,1) 
+        }
+        else{
+          b <- solve(t(x)%*%(w*Ai*x*wt))%*%t(x)%*%(w*Ai*zj*wt)
+        }
+        nj <- x%*%b+xa%*%ba
+        nj <- ifelse(nj>E^2,E^2,nj)
+        uj <- exp(nj)/(1+exp(nj))
+        olddev <- dev
+        uj <- ifelse(uj<E^-150,E^-150,uj)
+        tt <- y/uj
+        tt <- ifelse(tt==0,E^-10,tt)
+        uj <- ifelse(uj==1,0.99999,uj)
+        tt2 <- (1-y)/(1-uj)
+        tt2 <- ifelse(tt2=0,1E-10,tt2)
+        dev <- 2*sum((y*log(tt))+(1-y)*log(tt2))
+        ddev <- dev-olddev
+        cont2 <- cont2+1
+      } #fecha while
+      if(det(t(x)%*%(w*Ai*x*wt))){
+        C <- matrix(0,nvar,n)
+      }
+      else{
+        C <- solve(t(x)%*%(w*Ai*x*wt))*t(x)*t(w*Ai*wt)
+      }
+      R_matrix <- rbind(R_matrix,(x[i,]*C))
+      Z_matrix <- rbind(Z_matrix,(zj+xa*ba))
+      yhat[i] <- uj[i]
+    } # fecha o laço for
+    hat_matrix <- R_matrix*Z_matrix/diag(Z_matrix)
+    uj <- yhat
+    nj <- log(uj/(1-uj))
+    Aa <- uj*(1-uj)
+    Aa <- ifelse(Aa<=0,E^-5,Aa)
+    za <- nj+(y-uj)/Aa
+    if (det(t(xa%*%Aa)%*%(diag(1,n,n)-hat_matrix)*(xa*wt))==0){
+      ba <- matrix(0,ncol(xa),1)
     }
-  }  
+    else{
+      ba <- solve(t(xa*Aa)%*%(diag(1,n,n)-hat_matrix))%*%t(xa*Aa)%*%(diag(1,n,n)-hat_matrix)%*%(za*wt)
+    }
+    nj <- xa*ba
+    nj <- ifelse(nj>E^2,E^2,nj)
+    uj <- exp(nj)/(1+exp(nj))
+    diffba <- oldba-ba
+    contb <- contb+1
+    } #fecha while (diffba)
+    S <- diag((diag(1,n,n)-hat_matrix)%*%xa%*%solve(t(xa*Aa)%*%(diag(1,n,n)-hat_matrix)%*%(xa*wt))%*%t(xa*Aa*wt)%*%(diag(1,n,n)-hat_matrix)+hat_matrix)
+    uj <- ifelse(uj==0,E^-10,uj)
+    uj <- ifelse(uj==1,0.99999,uj)
+    CV <- t((y-yhat)*wt)%*%(y-yhat)
+    ll <- sum(y*log(uj)-(1-y)*log(1-uj))
+    npar <- sum(S)
+    AIC <-  2*npar-2*ll
+    AICC <-  AIC +(2*npar*(npar+1))/(n-npar-1)
+  } # fecha modelo logístico  
     
-  } #fecha CV
-} #fecha golden
+  } # fecha CV
+} # fecha golden
 
 # trocas ----
 # position ---- posit
@@ -251,6 +388,17 @@ teste <- exemplo[which(!exemplo==0)]  #retorna os valores
 
 
 # nao fazer para adaptiven
+
+
+
+
+
+
+
+
+
+
+
 
 
 
